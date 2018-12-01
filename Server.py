840c1a6416cc54847e0fd31f8f42a7b64f84c400
Uddhav Bhosle <ubhosle@ncsu.edu>
2018-12-01 18:05:40 -0500

    lost ack

--- Server.py
+++ Server.py
@@ -20,11 +20,13 @@ CURRENT_SEQUENCE_NUMBER = 0
 Window = []
 RTT = 0
 startRTTCalc = True
+repeatSeq = 0
+repeatSeqCount=0
 try:
     with open(filename, 'w') as outfile:
         while True:
             request, address = server_socket.recvfrom(1024)
-            print(request)
+ #           print(request)
             if request == b'END':
                 print("Done sending")
                 RTT = time.time() - RTT
@@ -38,13 +40,20 @@ try:
             data = extract_data(request)
             # print("calculated checksum: ", checksum, "received checksum: ", received_checksum)
             if CURRENT_SEQUENCE_NUMBER == int(data[0]) and (checksum == received_checksum or random.uniform(0, 1) > probability_of_loss):
-                print("DATA:\t",data[3].decode("utf-8"))
+                # print("DATA:\t",data[3].decode("utf-8"))
                 outfile.write(data[3].decode("utf-8"))
                 packet = Packet(int(data[0]), 43690)
                 server_socket.sendto(packet.packetData, (client_ip, client_port_number))
+                repeatSeq = CURRENT_SEQUENCE_NUMBER
                 CURRENT_SEQUENCE_NUMBER += 1
+                repeatSeqCount = 0
             else:
-                print("Packet loss, sequence number =", data[0])
+                if repeatSeq == data[0]:
+                        repeatSeqCount += 1
+                if repeatSeqCount > 5:
+                        packet = Packet(int(data[0]), 43690)
+                        server_socket.sendto(packet.packetData, (client_ip, client_port_number))
+                print("Packet loss, sequence number =", data[0], "curr_seq: ",CURRENT_SEQUENCE_NUMBER, " rece seq:", data[0], checksum, received_checksum)
     print("RTT: ", RTT)
 except Exception as e:
    print(e)
