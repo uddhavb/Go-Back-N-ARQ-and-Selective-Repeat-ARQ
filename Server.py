93abb11534dd89b86a63d0f970d0fc8aaaeef074
Uddhav Bhosle <ubhosle@ncsu.edu>
2018-12-03 15:32:54 -0500

    remove unncessary comments

--- Server.py
+++ Server.py
@@ -17,7 +17,6 @@ server_socket.bind(('', bind_port))
 server_socket.settimeout(10)
 # server.listen(5)  # max backlog of connections
 CURRENT_SEQUENCE_NUMBER = 0
-Window = []
 RTT = 0
 startRTTCalc = True
 repeatSeq = 0
@@ -26,7 +25,7 @@ try:
     with open(filename, 'w') as outfile:
         while True:
             request, address = server_socket.recvfrom(1024)
- #           print(request)
+           # print(request)
             if request == b'END':
                 print("Done sending")
                 RTT = time.time() - RTT
@@ -38,23 +37,14 @@ try:
             received_checksum = request[4]<<8
             received_checksum = received_checksum + request[5]
             data = extract_data(request)
-            # print("calculated checksum: ", checksum, "received checksum: ", received_checksum)
-            if CURRENT_SEQUENCE_NUMBER == int(data[0]) and (checksum == received_checksum and random.uniform(0, 1) > probability_of_loss):
-                # print("DATA:\t",data[3].decode("utf-8"))
+            if CURRENT_SEQUENCE_NUMBER == int(data[0]) and checksum == received_checksum and random.uniform(0, 1) > probability_of_loss:
                 outfile.write(data[3].decode("utf-8"))
                 packet = Packet(int(data[0]), 43690)
                 server_socket.sendto(packet.packetData, (client_ip, client_port_number))
-                repeatSeq = CURRENT_SEQUENCE_NUMBER
                 CURRENT_SEQUENCE_NUMBER += 1
-                repeatSeqCount = 0
             else:
-                if repeatSeq == data[0]:
-                        repeatSeqCount += 1
-                if repeatSeqCount > 5:
-                        packet = Packet(int(data[0]), 43690)
-                        server_socket.sendto(packet.packetData, (client_ip, client_port_number))
-                print("Packet loss, sequence number =", data[0], "curr_seq: ",CURRENT_SEQUENCE_NUMBER, " rece seq:", data[0], checksum, received_checksum)
-    print("RTT: ", RTT)
+                print("Packet loss, sequence number =", data[0])
+    # print("RTT: ", RTT)
 except Exception as e:
    print(e)
    print("Connection broken")
